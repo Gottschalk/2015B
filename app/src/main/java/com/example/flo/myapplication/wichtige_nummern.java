832ec982bd5af9eb83909306dd88de5235a13d3c
Gottschalk <florian.gottschalk@stud.uni-regensburg.de>
2015-01-27 21:13:05 +0100

    test

--- app/src/main/java/com/example/flo/myapplication/wichtige_nummern.java
+++ app/src/main/java/com/example/flo/myapplication/wichtige_nummern.java
@@ -17,6 +17,7 @@ public class wichtige_nummern extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wichtige_nummern);
         setupUI();
+        //
     }
 
     private void setupUI() {
