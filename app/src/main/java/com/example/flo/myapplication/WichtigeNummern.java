f83c12bc7c6ca06e5e9009cdd8c9e0ab7fc0e179
Gottschalk <florian.gottschalk@stud.uni-regensburg.de>
2015-02-02 14:08:48 +0100

    changed theme; prompt for number entry;

--- app/src/main/java/com/example/flo/myapplication/WichtigeNummern.java
+++ app/src/main/java/com/example/flo/myapplication/WichtigeNummern.java
@@ -1,7 +1,8 @@
 package com.example.flo.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -10,14 +11,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class WichtigeNummern extends ActionBarActivity {
+
+   private ArrayList<String> valueList = new ArrayList<String>();
+   private int listLength;
+   private ArrayAdapter adapter;
+   private final String[] CONTACT = new String[] {"ADAC", "ACE","AUTOMOBILCLUB","WERKSTATT","SPERRNUMMER_KREDITKARTE"};
+   private final String[] PHONE_NUMBER = new String[]{"*100#","*#0*#","*100#","*100#","*100#","116116"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +32,23 @@ public class WichtigeNummern extends ActionBarActivity {
         setContentView(R.layout.activity_wichtige_nummern);
         setupUI();
 
-        List valueList = new ArrayList<String>();
-        final String[] contact = new String[] {"ADAC", "ACE","AUTOMOBILCLUB","WERKSTATT","SPERRNUMMER_KREDITKARTE"};
-        final String[] phoneNumber = new String[]{"*100#","*#0*#","*100#","*100#","*100#","116116"};
-        for (int i = 0 ; i<contact.length ; i++){
-            valueList.add( contact[i] + ": " + phoneNumber[i]);
+
+        listLength = CONTACT.length;
+        for (int i = 0 ; i<CONTACT.length ; i++){
+            valueList.add(CONTACT[i] + ": " + PHONE_NUMBER[i]);
         }
         //
-        ListAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
-        final ListView lv = (ListView)findViewById(R.id.wichtige_nummern_listView);
+        final ListView LISTVIEW_NUMMERN = (ListView)findViewById(R.id.wichtige_nummern_listView);
+        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+        LISTVIEW_NUMMERN.setAdapter(adapter);
 
-        lv.setAdapter(adapter);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        LISTVIEW_NUMMERN.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(WichtigeNummern.this,WichtigeNummernDetailanzeige.class);
-                int clickedItemIndex = (int)lv.getAdapter().getItemId(position);
-                i.putExtra("NAME", contact[clickedItemIndex]);
-                i.putExtra("NUMMER", phoneNumber[clickedItemIndex]);
+                Intent i = new Intent(WichtigeNummern.this, WichtigeNummernDetailanzeige.class);
+                int clickedItemIndex = (int) LISTVIEW_NUMMERN.getAdapter().getItemId(position);
+                i.putExtra("NAME", CONTACT[clickedItemIndex]);
+                i.putExtra("NUMMER", PHONE_NUMBER[clickedItemIndex]);
                 startActivity(i);
             }
         });
@@ -51,7 +56,45 @@ public class WichtigeNummern extends ActionBarActivity {
     }
 
     private void setupUI() {
+        Button nummerHinzufuegen = (Button)findViewById(R.id.nummer_hinzufuegen_button);
+        nummerHinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                showPrompt();
+             // Item zu liste hinzufügen
+             //   valueList.add("test" +listLength++);
+             //   adapter.notifyDataSetChanged();
+
+
+            }
+        });
+    }
+
+    private void showPrompt() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Nummer hinzufügen:");
+        alert.setMessage("Nummer:");
+
+// Set an EditText view to get user input
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String value = String.valueOf(input.getText());
+                // Do something with value!
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
     }
 
 
