d6e869f99b320c45c81c61a7b0cfc91d49699ed1
Gottschalk <Florian.Gottschalk@stud.uni-regensburg.de>
2015-06-17 00:52:46 +0200

    menü entfernt; + imageflip element

--- app/src/main/java/com/example/flo/myapplication/GefahrenstelleAbsichernAnleitung.java
+++ app/src/main/java/com/example/flo/myapplication/GefahrenstelleAbsichernAnleitung.java
@@ -76,7 +76,7 @@ public class GefahrenstelleAbsichernAnleitung extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gefahrenstelle_absichern__anleitung, menu);
+        //getMenuInflater().inflate(R.menu.menu_gefahrenstelle_absichern__anleitung, menu);
         return true;
     }
 
