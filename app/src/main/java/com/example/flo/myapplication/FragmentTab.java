a70e848b2f45c67fc01686ef69a1021f4e6c5d02
Gottschalk <Florian.Gottschalk@stud.uni-regensburg.de>
2015-06-20 14:27:45 +0200

    tabview panne beheben

---
+++ app/src/main/java/com/example/flo/myapplication/FragmentTab.java
@@ -0,0 +1,32 @@
+package com.example.flo.myapplication;
+
+import android.annotation.TargetApi;
+import android.support.v4.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Flo on 20.06.2015.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class FragmentTab extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        tv.setText(this.getTag() + " Content");
+        return v;
+    }
+}
--- app/src/main/java/com/example/flo/myapplication/PanneBeheben.java
+++ app/src/main/java/com/example/flo/myapplication/PanneBeheben.java
@@ -1,6 +1,7 @@
 package com.example.flo.myapplication;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,10 +9,25 @@ import android.view.MenuItem;
 
 public class PanneBeheben extends ActionBarActivity {
 
+    private FragmentTabHost mTabHost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panne_beheben);
+
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab1").setIndicator("Tab 1", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator("Tab 2", null),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator("Tab 3", null),
+                FragmentTab.class, null);
     }
 
 
