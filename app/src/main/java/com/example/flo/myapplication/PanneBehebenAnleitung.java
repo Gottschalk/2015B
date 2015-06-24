fa4a82a6ca5a6bfe312f073d5a789c9533a06bec
Gottschalk <Florian.Gottschalk@stud.uni-regensburg.de>
2015-06-24 17:22:26 +0200

    fragments listview mit db werten befüllen; weiterleitung zu anleitung acitivity

---
+++ app/src/main/java/com/example/flo/myapplication/PanneBehebenAnleitung.java
@@ -0,0 +1,46 @@
+package com.example.flo.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PanneBehebenAnleitung extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panne_beheben_anleitung);
+
+        Intent i = getIntent();
+
+        String name = i.getStringExtra("NAME");
+
+        Log.w("pannebeheben anleitung ", name);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_panne_beheben_anleitung, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/flo/myapplication/PanneBehebenFragmentSymptomTab.java
+++ app/src/main/java/com/example/flo/myapplication/PanneBehebenFragmentSymptomTab.java
@@ -1,17 +1,26 @@
 package com.example.flo.myapplication;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 
 public class PanneBehebenFragmentSymptomTab extends Fragment {
+
+    private PanneDBHelper db;
+    private PanneAdapter adapter;
+    private ArrayList<Panne> pannen;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,15 +34,51 @@ public class PanneBehebenFragmentSymptomTab extends Fragment {
         View v = inflater.inflate(R.layout.fragment_layout_symptom, container, false);
         //  TextView tv = (TextView) v.findViewById(R.id.text);
         // tv.setText("Symptom");
-        ListView nameListView = (ListView) v.findViewById(R.id.panne_beheben_fragment_symptom_listview);
+        final ListView symptomListView = (ListView) v.findViewById(R.id.panne_beheben_fragment_symptom_listview);
 
 
-        String[] codeLearnChapters = new String[]{"Kontrolllampen leuchten", "Lenkung schwerfaellig",
-                "Motor ruckelt aber startet nicht", "Rauch aus dem Motorblock", "Auspuff laut"};
+        db = new PanneDBHelper(getActivity());
 
-        ArrayAdapter<String> codeLearnArrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, codeLearnChapters);
+        // falls zu viele testelemente in der db sind
+        //   db.deleteAllContacts();
 
-        nameListView.setAdapter(codeLearnArrayAdapter);
+        pannen = db.getAllPannen();
+
+        String[] symptomArray = new String[pannen.size()];
+
+        int index = 0;
+
+        for (Panne panne : pannen) {
+          //   String log = "Id: " + panne.getId() + " ,Name: " + panne.getName() + " ,Ursache: " + panne.getUrsache() + "Symptom" + panne.getSymptom();
+          //  Log.e("SymptomFragment: ", log + "  / index: " + index);
+          //      Log.e("SymptomFragment: " , panne.getSymptom());
+
+            symptomArray[index] = panne.getSymptom();
+            index++;
+        }
+
+        ArrayAdapter<String> codeLearnArrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, symptomArray);
+
+        symptomListView.setAdapter(codeLearnArrayAdapter);
+
+        symptomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getActivity() , PanneBehebenAnleitung.class);
+                int clickedItemIndex = (int) symptomListView.getAdapter().getItemId(position);
+
+                String panneName = pannen.get(position).getName();
+
+
+                int symptomId = pannen.get(position).getId();
+
+                Log.w("###############nr:", String.valueOf(symptomId));
+
+                i.putExtra("NAME", panneName);
+
+                startActivity(i);
+            }
+        });
         return v;
     }
 }
